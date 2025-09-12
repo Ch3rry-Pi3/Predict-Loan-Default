95a59cf44434c112d0fcdd6ef97ba12c0a5d1709
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-09-12 11:24:21 +0100

    a lot of functionality for run trial copied over from keras trial function

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -372,6 +372,52 @@ def run_xgb_trial(
 ) -> Dict[str, Any]:
     """Run a single randomised-search trial for XGBoost and log to MLflow"""
     pass
+    # Base esitmator
+    clf = XGBClassifier(
+        objective="binary:logistic",
+        eval_metric="auc",
+        tree_method="hist",
+        enable_categorical=False,
+        random_state=int(rng.integers(0, 1_000_000))
+    )
+
+    # Define random search space
+    dist = {
+        "n_estimators": [200, 300, 500],
+        "max_depth": [3, 4, 6],
+        "learning_rate": [0.03, 0.05, 0.1],
+        "subsample": [0.8, 1.0],
+        "colsample_bytree": [0.8, 1.0],
+        "min_child_weight": [1, 5],
+        "reg_lambda": [0.5, 1.0, 2.0],
+        "reg_alpha": [0.0, 0.1],
+    }
+    
+    # Use small CV to reduce runtime; shuffle for robustness
+    search_random_state = int(rng.integers(0, 1_000_000))
+    cv = StratifiedKFold(n_splits=N_FOLDS, shuffle=True, random_state=RANDOM_STATE)
+
+    with mlflow.start_run(run_name=f"XGB_trial_{trial_idx + 1}", nested=True):
+
+        # Tag run for discoverability
+        mlflow.set_tags({"model": "XGBoost", "trial": trial_idx + 1})
+
+        # Log XGBoost training curves; log model manually later
+        mlflow.xgboost.autolog(log_models=False) 
+
+        search = RandomizedSearchCV(
+            estimator=clf,
+            param_distributions=dist,
+            n_iter=1,
+            scoring="roc_auc",
+            cv=cv,
+            random_state=search_random_state,
+            n_jobs=-1,
+            verbose=1,
+            refit=True,
+        )
+    
+    
 
 # -------------------------------------------------------------------
 # Keras (Neural Network) Trial
@@ -448,7 +494,7 @@ def run_keras_trial(
             estimator=clf,
             param_distributions=dist,
             n_iter=1,
-            scoring="accuracy",     
+            scoring="roc_auc",     
             cv=cv,
             random_state=search_random_state,
             n_jobs=-1,              
